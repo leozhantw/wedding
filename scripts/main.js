4aff914b397d16c1e8711fd16f91e168ffba40f0
LeoZhan <leo3838ok@gmail.com>
2022-04-09 16:07:08 +0800

    refactor: change to use lazyload.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ let $grid;
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 $(document).ready(function() {
+  lazyload();
+
   $grid = $('.grid').isotope({
     itemSelector: '.grid-item',
     masonry: {
